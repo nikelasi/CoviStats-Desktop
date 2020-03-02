f20c348d82e0b19aa57a716d73aa991207861c86
NJ889 <58451243+NicholasJohansan@users.noreply.github.com>
2020-03-02 22:21:59 +0800

    Add files via upload

--- COVID19-STATS.py
+++ COVID19-STATS.py
@@ -13,56 +13,82 @@ mainFont = font.Font(family="times", size=10)
 
 def getData():
 
-	result = requests.get("https://www.moh.gov.sg/covid-19")
-	src = result.content
-	soup = BeautifulSoup(src, 'html.parser')
+	try:
+		result = requests.get("https://www.moh.gov.sg/covid-19")
+		src = result.content
+		soup = BeautifulSoup(src, 'html.parser')
 
 
-	temptags = []
-	intdata = []
-	DORSCON_color = ""
+		temptags = []
+		intdata = []
+		DORSCON_color = ""
 
-	##HOSPITALISED
-	for tag in soup.select('tr > td > p > font > span > b'):
-	    temptags.append(tag)
+		##HOSPITALISED [0] - STABLE [1] - DEATH [2] - critical [0] - [1]
+		for tag in soup.select('tr > td > span > strong > span'):
+			temptags.append(tag)
 
-	##DISCHARGED - TOTAL CONFIRMED
-	for tag in soup.select('tr > td > p > font > b'):
-	    temptags.append(tag)
+		##DISCHARGED [3] - total [3] + [0]
+		for tag in soup.select('tr > td > strong > span'):
+			temptags.append(tag)
 
-	##Integer Stats
-	for tag in temptags:
-	    try:
-	        intdata.append(int("".join((tag.text).split())))
-	    except:
-	        pass
+		##Integer Stats
+		for tag in temptags:
+			try:
+				intdata.append(int("".join((tag.text).split())))
+			except:
+				pass
 
-	if intdata == []:
-	    intdata = ["N/A", "N/A", "N/A"]
+		try:
+			hospitalised = intdata[0]
+			stable = intdata[1]
+			critical = intdata[0] - intdata[1]
+			death = intdata[2]
+			discharged = intdata[3]
+			total = intdata[3] + intdata[0]
+		except:
+			hospitalised = "N/A"
+			stable = "N/A"
+			critical = "N/A"
+			death = "N/A"
+			discharged = "N/A"
+			total = "N/A"
 
-	##DORSCON COLOR
-	for tag in soup.select('div > table > tbody > tr > td > h4 > strong > span')[1:]:
-	    if tag.text == "Green":
-	        DORSCON_color = "#ABE553"
-	    elif tag.text == "Yellow":
-	        DORSCON_color = "#FFCD00"
-	    elif tag.text == "Orange":
-	        DORSCON_color = "#FF7D01"
-	    elif tag.text == "Red":
-	        DORSCON_color = "#F85A4F"
-	    else:
-	        DORSCON_color = "#D5D8D7"
+		try:
+			for tag in soup.select('div > table > tbody > tr > td > h4 > strong > span')[1:]:
+				if tag.text == "Green":
+					DORSCON_color = "#ABE553"
+				elif tag.text == "Yellow":
+					DORSCON_color = "#FFCD00"
+				elif tag.text == "Orange":
+					DORSCON_color = "#FF7D01"
+				elif tag.text == "Red":
+					DORSCON_color = "#F85A4F"
+				else:
+					DORSCON_color = "#D5D8D7"
+		except:
 
-	if DORSCON_color == "":
-	    DORSCON_color = "#D5D8D7"
+			DORSCON_color = "#D5D8D7"
+	except:
+		hospitalised = "N/A"
+		stable = "N/A"
+		critical = "N/A"
+		death = "N/A"
+		discharged = "N/A"
+		total = "N/A"
 
-	cfmcasesInt.config(text=intdata[2])
+		DORSCON_color = "#D5D8D7"
 
-	hospitalisedInt.config(text=intdata[0])
+	cfmcasesInt.config(text=total)
 
-	dischargedInt.config(text=intdata[1])
+	dischargedInt.config(text=discharged)
 
-	deadInt.config(text=0)
+	hospitalisedInt.config(text=hospitalised)
+
+	stableInt.config(text=stable)
+
+	criticalInt.config(text=critical)
+
+	deathInt.config(text=death)
 
 	DORSCONLbl.config(background=DORSCON_color)
 
@@ -75,26 +101,36 @@ cfmcasesLbl.grid(row=1, column=0, sticky=W)
 cfmcasesInt = Label(main, text="Number", font=mainFont, padx=10, pady=5)
 cfmcasesInt.grid(row=1, column=1, sticky=W)
 
-hospitalisedLbl = Label(main, text="Hospitalised:", font=mainFont, padx=10, pady=5)
-hospitalisedLbl.grid(row=2, column=0, sticky=W)
-hospitalisedInt = Label(main, text="Number", font=mainFont, padx=10, pady=5)
-hospitalisedInt.grid(row=2, column=1, sticky=W)
-
 dischargedLbl = Label(main, text="Discharged:", font=mainFont, padx=10, pady=5)
-dischargedLbl.grid(row=3, column=0, sticky=W)
+dischargedLbl.grid(row=2, column=0, sticky=W)
 dischargedInt = Label(main, text="Number", font=mainFont, padx=10, pady=5)
-dischargedInt.grid(row=3, column=1, sticky=W)
+dischargedInt.grid(row=2, column=1, sticky=W)
 
-deadLbl = Label(main, text="Deaths:", font=mainFont, padx=10, pady=5)
-deadLbl.grid(row=4, column=0, sticky=W)
-deadInt = Label(main, text="Number", font=mainFont, padx=10, pady=5)
-deadInt.grid(row=4, column=1, sticky=W)
+hospitalisedLbl = Label(main, text="Hospitalised:", font=mainFont, padx=10, pady=5)
+hospitalisedLbl.grid(row=3, column=0, sticky=W)
+hospitalisedInt = Label(main, text="Number", font=mainFont, padx=10, pady=5)
+hospitalisedInt.grid(row=3, column=1, sticky=W)
+
+stableLbl = Label(main, text="Stable Cases:", font=mainFont, padx=10, pady=5)
+stableLbl.grid(row=4, column=0, sticky=W)
+stableInt = Label(main, text="Number", font=mainFont, padx=10, pady=5)
+stableInt.grid(row=4, column=1, sticky=W)
+
+criticalLbl = Label(main, text="Critical Cases:", font=mainFont, padx=10, pady=5)
+criticalLbl.grid(row=5, column=0, sticky=W)
+criticalInt = Label(main, text="Number", font=mainFont, padx=10, pady=5)
+criticalInt.grid(row=5, column=1, sticky=W)
+
+deathLbl = Label(main, text="Deaths:", font=mainFont, padx=10, pady=5)
+deathLbl.grid(row=6, column=0, sticky=W)
+deathInt = Label(main, text="Number", font=mainFont, padx=10, pady=5)
+deathInt.grid(row=6, column=1, sticky=W)
 
 DORSCONLbl = Label(main, text="DORSCON", font=mainFont, padx=10, pady=5, width=30)
-DORSCONLbl.grid(row=5, column=0, columnspan=3)
+DORSCONLbl.grid(row=7, column=0, columnspan=3)
 
 updateBtn = Button(main, text="Update", font=mainFont, padx=10, pady=5, command=getData, width=30)
-updateBtn.grid(row=6, column=0, columnspan=3)
+updateBtn.grid(row=8, column=0, columnspan=3)
 
 getData()
 
